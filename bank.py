9a5c88005e36bcd6e8b260c726ee9ada85c4b658
YourNameple.com <you@example.com>
2024-01-17 10:12:15 +0200

    bank

--- bank.py
+++ bank.py
@@ -17,13 +17,25 @@ while True:
     choice = input("Enter your choice (1-4): ")
 
     if choice == '1':
-        # papildini kodu šeit
+        deposit=int(input("How much do you want to deposit?"))
+        if (deposit > 0):
+            balance=balance + deposit
+        elif(deposit <= 0):
+            print("Invalid value! Value cannot be zero or negative")
+        else:
+            print("Enter only numbers")
         pass
     elif choice == '2':
-        # papildini kodu šeit
+        withdraw=int(input("How much do you want to withdraw?"))
+        if (withdraw<=0):
+            print("Invalid value! Value cannot be zero or negative")
+        elif(withdraw>balance):
+            print("You don't have enough money on your balance")
+        else:
+            balance=balance - withdraw
         pass
     elif choice == '3':
-        # papildini kodu šeit
+        print("Your current balance:", balance)
         pass
     elif choice == '4':
         print("Exiting the banking system. Thank you!")
